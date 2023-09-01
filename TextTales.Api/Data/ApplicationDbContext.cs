42537e620ca659ed2972b379bc4ac0aa6f26a912
Borna Juraski <borna.juraski@outlook.com>
2023-09-01 18:20:51 +0200

    added api project and EF setup

---
+++ TextTales.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TextTales.Api.Entities;
+
+namespace TextTales.Api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
---
+++ TextTales.Api/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextTales.Api.Entities;
+
+public class Category
+{
+    public long Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    [Range(1, 50, ErrorMessage = "The Display Order value must be between 1 and 50.")]
+    public int DisplayOrder { get; set; }
+}
